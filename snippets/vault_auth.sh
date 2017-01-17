85c72bcedf9dea0a785ca9420e9da64815080d4c
mtaracha <marcintaracha1991@gmail.com>
2017-01-17 23:46:09 +0000

    Add best practises elements

--- snippets/vault_auth.sh
+++ snippets/vault_auth.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#
+# Author: Marcin Taracha
+#
 # Based on
 # https://www.vaultproject.io/intro/getting-started/apis.html
 # Requirements: 
@@ -7,8 +9,18 @@
 # - Vault AppRole authentication enabled (role-id  & secret-id)
 #
 # Comments
-# - make sure that you have secure your vault server to access only from certain IPs
+# - make sure that you have secure your vault server to be accessible only from certain IPs (i.e. db/web servers, workstation)
 
+set -o errexit # script exit when a command fails
+set -o pipefail # exit status of the last command that threw a non-zero exit code is returned
+set -o nounset # exit when your script tries to use undeclared variables
+set -o xtrace # trace what gets executed
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
 
 vault_server_endpoint="7.7.7.73:8200"
 role_id="7f538aca-bcdc-f6e1-fec8-2b6d8689a480"
