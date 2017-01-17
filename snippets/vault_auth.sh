f2bf4758360e9ebe04c5107b3ac0c31b65c99b99
mtaracha <marcintaracha1991@gmail.com>
2017-01-17 23:25:22 +0000

    Get token from vault

---
+++ snippets/vault_auth.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Based on
+# https://www.vaultproject.io/intro/getting-started/apis.html
+# Requirements: 
+# - packages: jq
+# - AppRole authentication enabled (role-id  & secret-id)
+
+vault_server_endpoint="7.7.7.73:8200"
+role_id="7f538aca-bcdc-f6e1-fec8-2b6d8689a480"
+secret_id="d3d156fc-2a0e-07dd-0aea-3e5b20705abd"
+
+client_token=$(curl -s -X POST \
+     -d '{"role_id":"'"$role_id"'","secret_id":"'"$secret_id"'"}' \
+     http://${vault_server_endpoint}/v1/auth/approle/login | jq -r .auth.client_token)
+
+echo $client_token
