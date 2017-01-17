d8c1948a46fdc040172c1dd22c9c2cdd4ca82570
mtaracha <marcintaracha1991@gmail.com>
2017-01-17 23:28:14 +0000

    Get vault secret

--- snippets/vault_auth.sh
+++ snippets/vault_auth.sh
@@ -9,9 +9,10 @@
 vault_server_endpoint="7.7.7.73:8200"
 role_id="7f538aca-bcdc-f6e1-fec8-2b6d8689a480"
 secret_id="d3d156fc-2a0e-07dd-0aea-3e5b20705abd"
+secret_path="github/access_token"
 
-client_token=$(curl -s -X POST \
+vault_token=$(curl -s -X POST \
      -d '{"role_id":"'"$role_id"'","secret_id":"'"$secret_id"'"}' \
      http://${vault_server_endpoint}/v1/auth/approle/login | jq -r .auth.client_token)
 
-echo $client_token
+curl -s -X GET -H "X-Vault-Token:$vault_token" http://$vault_server_endpoint/v1/secret/$secret_path | jq -r .data.value
