d55f00a5a55e603aaa830a52ca519f61b1bf1c42
mtaracha <marcintaracha1991@gmail.com>
2017-01-22 01:36:33 +0000

    Complete script

--- snippets/vault_auth.py
+++ snippets/vault_auth.py
@@ -2,6 +2,7 @@
 
 import argparse
 import requests
+import json
 
 parser = argparse.ArgumentParser(description='Vault auth/get')
 parser.add_argument('-s', '--server', help='[-s <vault_ip>:<port>]', required=True)
@@ -18,42 +19,34 @@ class Vault(object):
 		self.secret_path = args.secret_path
 		self.role_id = args.role_id
 		self.secret_id = args.secret_id
-
+	"""Get vault token to authenticate"""
 	def vault_auth(self):
-		#vault_token = requests.post('http://' + self.vault_server_endpoint + '/v1/auth/approle/login', data = {"role_id": self.role_id,"secret_id":self.secret_id})
-		#vault_token = requests.post("http://7.7.7.73:8200/v1/auth/approle/login", data = '{"role_id": "7f538aca-bcdc-f6e1-fec8-2b6d8689a480", "secret_id": "d3d156fc-2a0e-07dd-0aea-3e5b20705abd"}')
-		#print(vault_token.status_code, vault_token.reason)
-		#r = requests.post("http://bugs.python.org", data={'number': 12524, 'type': 'issue', 'action': 'show'})
-		#print(r.status_code, r.reason)
-		#return r
-		#return vault_token.text
+
 		data = ('{"role_id": "%s" ,"secret_id":"%s"}' % (self.role_id, self.secret_id))
 		try:
-			vault_token = requests.post('http://' + self.vault_server_endpoint + '/v1/auth/approle/login', data = data)
+			vault_token_request = requests.post('http://' + self.vault_server_endpoint + '/v1/auth/approle/login', data = data)
 
-			if not vault_token.status_code // 100 == 2:
-				return "Error: Unexpected response {}".format(vault_token.status_code)
+			if not vault_token_request.status_code // 100 == 2:
+				return "Error: Unexpected response {}".format(vault_token_request.status_code)
 
-			#print self.role_id
-			return vault_token.text
+			vault_token = json.loads(vault_token_request.text)
+			return vault_token['auth']['client_token']
 
 
 		except requests.exceptions.RequestException as e:
 			return "Error: {}".format(e)        
+	"""Get vault vault secret using token"""
+	def vault_get_secret(self):
 
-
-
-#	def vault_get_secret():
-#		return 0
-
-#'curl -s -X GET -H "X-Vault-Token:$vault_token" http://$vault_server_endpoint/v1/$secret_path | jq -r .data.value
-
+		vault_token = self.vault_auth()
+		vault_secret_data = requests.get("http://" + self.vault_server_endpoint + "/v1/" + self.secret_path, headers={"X-Vault-Token":vault_token})
+		vault_secret = json.loads(vault_secret_data.text)
+		return vault_secret['data']['value']
 
 def args_check():
 	return 0
 
 if __name__ == '__main__':
-	#print vault_server_endpoint
+
    	vault = Vault(args)
-   	print vault.vault_auth()
-   	#vault.vault_auth()
+   	print vault.vault_get_secret()
