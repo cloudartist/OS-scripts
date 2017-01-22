453e9ac2078bc21e08ed4de0dd7eb33d4b3cbf40
mtaracha <marcintaracha1991@gmail.com>
2017-01-22 01:07:39 +0000

    Finish basic updatesg script

--- bash/update_aws_sg.sh
+++ bash/update_aws_sg.sh
@@ -11,6 +11,7 @@
 # Comments
 # - Script changes SG rules to reflect ISP IP alteration
 # - Recommend to put script as cron job
+# Example: ./update_aws_sg.sh -s sg-f7ab3691 -p 1194 -r eu-west-1
 
 
 set -o errexit 
@@ -18,9 +19,33 @@ set -o pipefail
 set -o nounset 
 set -o xtrace 
 
+usage() { echo "Usage: $0 [-s <security-group-id>] [-p <port number>] [-r <region>] [-S <secret_id>]" 1>&2; exit 1; }
+
+while getopts ":s:p::r:" o; do
+  case ${o} in
+    s) 
+		s=${OPTARG}
+		;;
+    p) 
+		p=${OPTARG}
+		;;
+    r) 
+		r=${OPTARG}
+		;;
+    *) 
+		usage
+		;;
+  esac
+done
+shift $((OPTIND-1)) 
+
 # Global variables
 
+security_group_id=${s}
+port_number=${p}
+region=${r}
 isp_ip=$(curl -s http://api.ipify.org)"/32"
+
 
 function check_compare_ip()
 {
@@ -31,7 +56,7 @@ function check_compare_ip()
 function modify_sg()
 {
 	# Check if any rules for $port exists
-	vpn_cidrs=$(aws ec2 describe-security-groups --group-ids sg-f7ab3691 --region eu-west-1  | jq -r '.SecurityGroups[0].IpPermissions[] | select(.ToPort == 1194) | .IpRanges[]' | jq .CidrIp)
+	vpn_cidrs=$(aws ec2 describe-security-groups --group-ids $security_group_id --region $region  | jq --arg testing "$(eval printf '%s' $port_number)" '.SecurityGroups[0].IpPermissions[] | select(.ToPort==1194) | .IpRanges[]' | jq .CidrIp)
 	# Remove unnecessary quotes
 	vpn_cidrs=$(echo $vpn_cidrs | sed 's/"//g')
 
@@ -46,12 +71,12 @@ function modify_sg()
 		do
 			:
 			echo "Removing rule for $cidr"
-			aws ec2 revoke-security-group-ingress --group-id sg-f7ab3691 --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 1194, "ToPort": 1194, "IpRanges": [{"CidrIp": "'"${cidr}"'"}]}]' --region eu-west-1
+			aws ec2 revoke-security-group-ingress --group-id $security_group_id --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 1194, "ToPort": 1194, "IpRanges": [{"CidrIp": "'"${cidr}"'"}]}]' --region eu-west-1
 		done
 	fi
-
+	echo $port_number
 	# Add rule with correct IP
-	aws ec2 authorize-security-group-ingress --group-id sg-f7ab3691 --protocol tcp --port 1194 --cidr $isp_ip --region eu-west-1
+	aws ec2 authorize-security-group-ingress --group-id $security_group_id --protocol tcp --port 1194 --cidr $isp_ip --region $region
 }
 
 function main()
